4b03c62b40c41f341ddc96206563596d1d7b98e1
rnagai <rnagai@hepburn.s.chiba-u.ac.jp>
2022-02-22 13:49:44 +0900

    LED analysis support

--- analysis/QLED_scan.py
+++ analysis/QLED_scan.py
@@ -1,12 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.ticker as ticker
 import tables 
 import click
 import os, sys
 from scipy.signal import argrelmax
+from natsort import natsorted
 
-@click.command()
+lws = [1,2,1,1,2,1,1,2,1,1,2,1] # downward bold
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('--filepath',type=str,required=True)
 @click.option('--xmax',default=None)
 @click.option('--rebin',type=int,default=1)
@@ -32,8 +40,6 @@ def main(filepath, xmax, rebin, log, gain):
     else:
         factor = 1
         xlabel = 'Charge [pC]'
-
-    lws = [1,2,1,1,2,1,1,2,1,1,2,1] # downward bold
     
     binwidth = 0.1 * rebin
     maxcharge = int(np.max(charges)/factor/binwidth+1)*binwidth
@@ -53,5 +59,84 @@ def main(filepath, xmax, rebin, log, gain):
     plt.savefig(f'{filepath}/chargehist.pdf')
     plt.show()
 
+@cli.command()
+@click.option('--filepath',type=str,required=True)
+def vscan(filepath):
+    fig = plt.figure(figsize=(8,4.8))
+    filename = f'{filepath}/raw/test.hdf5'
+    f = tables.open_file(filename)
+    data = f.get_node('/data')
+    channel = data.col('channel')
+    hv = data.col('hv')
+    charges = data.col('chargestamp')
+    ledbias = data.col('ledbias')
+    ledmask = data.col('ledmask')
+    meancharge = np.mean(charges,axis=1)
+    f.close()
+    for i in range(12):
+        if len(ledbias[np.log2(ledmask)==i]) > 0:
+            plt.plot(ledbias[np.log2(ledmask)==i],meancharge[np.log2(ledmask)==i],lw=lws[i],label=f'LED{i+1}',color=cm.brg(i/12),ls='--')
+
+    plt.title(f'Ch{channel[0]}, HV {np.mean(hv):.1f} V')
+    plt.xlabel('Intensity Setting (hex)')
+    plt.ylabel('Observed Mean Charge [pC]')
+    plt.legend(bbox_to_anchor=(1,1), loc='upper left')
+    
+    axes = plt.gca()
+    axes.get_xaxis().set_major_locator(ticker.MultipleLocator(2*16**2))
+    axes.get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: '%x' % int(x)))
+    
+    plt.tight_layout()
+    plt.savefig(f'{filepath}/LEDlumi.pdf')
+    plt.yscale('log')
+    plt.savefig(f'{filepath}/LEDlumi_log.pdf')
+    plt.show()
+
+@cli.command()
+@click.option('--filepath',type=str,required=True)
+def vscan_old(filepath):
+    fig = plt.figure(figsize=(8,4.8))
+    flist = natsorted(os.listdir(filepath))
+    vflist = [f for f in flist if os.path.isdir(os.path.join(filepath, f))]
+    qmeans = np.zeros((12,len(vflist)))
+    masks = np.zeros(12)
+    bias  = np.zeros((12,len(vflist)))
+    for i, fname in enumerate(vflist):
+        filename = f'{filepath}/{fname}/raw/test.hdf5'
+        f = tables.open_file(filename)
+        data = f.get_node('/data')
+        charges = data.col('chargestamp')
+        meancharges = np.mean(charges,axis=1)
+        ledbiass = data.col('ledbias')
+        ledmasks = data.col('ledmask')
+        ledperiods = data.col('ledperiod')
+        f.close()
+        for j in range(12):
+            qmeans[j][i] = np.mean(charges[j])
+            masks[j] = ledmasks[j]
+            bias[j][i] = ledbiass[j]
+
+    for i in range(12):
+        plt.plot(bias[i],qmeans[i],marker='o',lw=lws[i],label=f'LED{i+1}',color=cm.brg(i/12),ls='--')
+
+    plt.xlabel('Intensity Setting (hex)')
+    plt.ylabel('Observed Mean Charge [pC]')
+    plt.legend(bbox_to_anchor=(1,1), loc='upper left')
+    
+    #def to_hex(x, pos):
+    #    return '%x' % int(x)
+
+    fmt = ticker.FuncFormatter(lambda x: '%x' % int(x))
+    
+    axes = plt.gca()
+    axes.get_xaxis().set_major_locator(ticker.MultipleLocator(2*16**2))
+    axes.get_xaxis().set_major_formatter(fmt)
+    
+    plt.tight_layout()
+    plt.savefig(f'{filepath}/LEDlumi.pdf')
+    plt.yscale('log')
+    plt.savefig(f'{filepath}/LEDlumi_log.pdf')
+    plt.show()
+
 if __name__=='__main__':
-    main()
+    cli()
